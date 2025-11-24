c60b5fc3d8940772ade22602f81bfa7c3a195263
KaiiGritt <johnloydserapion96@gmail.com>
2025-11-24 18:13:41 +0800

    Update Recipe model to use Text for instructions and ingredients; update seed script

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from '@prisma/client'
+import { adapter } from '../../../prisma/prisma.config'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
