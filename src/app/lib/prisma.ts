bb764b7b876c6290b547fae0aee14101ee9334cf
KaiiGritt <johnloydserapion96@gmail.com>
2025-11-24 18:20:03 +0800

    Update prisma.ts

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,10 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-import { adapter } from '../../../prisma/prisma.config'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
