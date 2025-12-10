7100d372c9e3a27457b52bf60025162a95fcf74b
KaiiGritt <johnloydserapion96@gmail.com>
2025-12-10 18:01:13 +0800

    deprecation issue fix during build

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { prisma } from "../prisma/prisma.config";
+
+async function main() {
+  const users = await prisma.user.findMany();
+  console.log(users);
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
